8e7a6376c3b7a1919724c2ad7589dbf8b453a26e
mta343422 <oliveiramateus343@gmail.com>
2025-02-04 02:45:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+// Variáveis
+
+float precoVenda, valorImposto, valorDesconto, precoFinal;
+
+// Constantes
+
+const float taxaImposto = 0.1;
+
+const float descontoPadrao = 0.05;
+
+// Entrada
+
+printf("Informe o preço de venda do produto: ");
+
+scanf("%f", &precoVenda);
+
+// Processamento
+
+valorImposto = precoVenda * taxaImposto;
+
+valorDesconto = precoVenda * descontoPadrao;
+
+precoFinal = precoVenda + valorImposto - valorDesconto;
+
+// Saída
+
+printf("O preço final do televisor é: R$ %.2f", precoFinal);
+
+return 0;
+
+}
